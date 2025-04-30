b47c854579dce5e6cf0d1fa40fe53b86701a5074
Eryk Styczynski <eryk.styczynski2@mail.dcu.ie>
2025-05-01 00:43:43 +0200

    Merge build.gradle.kts and include bouncycastle in module-info.java

--- src/src/main/java/module-info.java
+++ src/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module ie.dcu.secureYAC {
     requires javafx.fxml;
     requires org.json;
     requires java.desktop;
+    requires org.bouncycastle.provider;
 
 
     opens ie.dcu.secureYAC to javafx.fxml;
